58a3e8a639b90cd5b17cbf5737f62901d96a8ad5
Shane Miller <shanemiller89@gmail.com>
2019-11-15 12:34:09 -0600

    testing

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -3,33 +3,57 @@ defmodule Cards do
     Provides methods for creating and handling a deck of cards.
   """
 
+  @doc """
+    Returns a list of strings representing a deck of playing cards.
+  """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
 
-    for  suit <- suits, value <- values do
-        "#{value} of #{suit}"
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
     end
 
     # List.flatten(cards)
-
   end
 
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
 
+  @doc """
+    Determines whether a deck contains a given card
+
+    ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> Cards.contains?(deck, "Ace of Spades")
+      true
+
+  """
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
 
+  @doc """
+  Divides a deck inyo a hand and the remainder of the deck.
+  The `hand_size` argument indicates how many cards should be in the hand.
+
+  ## Examples
+
+      iex> deck = Cards.create_deck()
+      iex> {hand, deck} = Cards.deal(deck, 1)
+      iex> hand
+      ["Ace of Spades"]
+
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
 
-  def save(deck,filename) do
+  def save(deck, filename) do
     binary = :erlang.term_to_binary(deck)
-    File.write(filename,binary)
+    File.write(filename, binary)
   end
 
   def load(filename) do
@@ -45,17 +69,15 @@ defmodule Cards do
       {:ok, binary} -> :erlang.binary_to_term(binary)
       # _ In front of variable names lets elixir know to ignore the variable
       {:error, _reason} -> "That file does not exist."
-
     end
   end
 
   def create_hand(hand_size) do
-    Cards.create_deck
+    Cards.create_deck()
     # Pipe operator (Uses | and >)
-    |>Cards.shuffle
-    #Elixer will automaticaly return the value as the first argument with Pipe operators
-    #Pipe operators are expecting you to write methods with consistent first arguments
-    |>Cards.deal(hand_size)
+    |> Cards.shuffle()
+    # Elixer will automaticaly return the value as the first argument with Pipe operators
+    # Pipe operators are expecting you to write methods with consistent first arguments
+    |> Cards.deal(hand_size)
   end
-
 end
